acdc36c0e4f63b247459e2e802017c63a8ff1641
Adrian Nads Marcelo <marcelonads@gmail.com>
2024-08-04 21:37:25 +0800

    learn: Database connection using JDBC and HikariCP

---
+++ src/com/fundamentals/sqlpractice/DatabaseConnection.java
@@ -0,0 +1,47 @@
+package com.fundamentals.sqlpractice;
+
+import java.sql.*;
+
+public class DatabaseConnection {
+
+    public static void main(String[] args) {
+
+        String url = "jdbc:mysql://localhost:3306/parkncharge_v3";
+        String username = "root";
+        String password = "4332wurx";
+        String query = "" +
+                "SELECT" +
+                "*" +
+                "FROM" +
+                "users WHERE username = ?";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }
+        catch(ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Connection conn = DatabaseConfig.getDataSource().getConnection();
+
+            DatabaseConfig.printPoolStats(DatabaseConfig.getDataSource());
+
+            PreparedStatement stmt = conn.prepareStatement(query);
+
+            stmt.setString(1, "httpdotjs");
+
+            ResultSet result = stmt.executeQuery();
+
+            while (result.next()) {
+
+                System.out.println(result.getString("username") + " " + result.getString("password"));
+            }
+
+            DatabaseConfig.printPoolStats(DatabaseConfig.getDataSource());
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
